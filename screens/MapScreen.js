276d0f9197f333c2bbd5900c9d230c2722c9eb22
Elvinas Lukasevicius <ell@open.ch>
2019-05-02 15:19:42 +0200

    Maps view with user location

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,9 +1,37 @@
 import React, { Component } from 'react';
 import { Container, Header, Title, Body, Right, Text, Button } from 'native-base';
 import { StyleSheet } from "react-native";
-import { MapView } from 'expo';
+import { MapView, Location, Permissions } from 'expo';
 
 export default class ListExample extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            region: null,
+        };
+
+        this._getLocationAsync();
+    }
+
+    _getLocationAsync = async () => {
+        let { status } = await Permissions.askAsync(Permissions.LOCATION);
+        if (status !== 'granted') {
+            this.setState({
+                errorMessage: 'Permission to access location was denied',
+            });
+        }
+
+        let location = await Location.getCurrentPositionAsync({ enableHighAccuracy: true });
+        let region = {
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude,
+            latitudeDelta: 0.045,
+            longitudeDelta: 0.045,
+        };
+        this.setState({ region });
+    };
+
 
     _finishTask() {
         console.log('finished');
@@ -13,6 +41,7 @@ export default class ListExample extends Component {
 
     render() {
         const { navigation } = this.props;
+        const { region } = this.state;
         const address = navigation.getParam('address', '');
         return (
             <Container>
@@ -31,12 +60,10 @@ export default class ListExample extends Component {
                 </Header>
                 <MapView
                     style={styles.mapContainer}
-                    initialRegion={{
-                        latitude: 37.78825,
-                        longitude: -122.4324,
-                        latitudeDelta: 0.0922,
-                        longitudeDelta: 0.0421,
-                    }}
+                    initialRegion={region}
+                    showsUserLocation={true}
+                    followsUserLocation={true}
+                    showsMyLocationButton={true}
                 />
             </Container>
         );
