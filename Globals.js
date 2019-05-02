90302c8808d058ec5aa7da30eb163a7061c8ac95
Elvinas Lukasevicius <ell@open.ch>
2019-05-02 23:20:20 +0200

    Start ignoring Globals.log

--- Globals.js
+++
@@ -1,8 +0,0 @@
-class Globals {
-    constructor() {
-        this.SERVER_URL = 'http://10.0.2.2';
-        this.GOOGLE_DIRECTIONS_API = 'AIzaSyCYvMpmVhFc0ydILEuXGJNYNGFnBoKPCL8';
-    }
-}
-
-export default (new Globals);
